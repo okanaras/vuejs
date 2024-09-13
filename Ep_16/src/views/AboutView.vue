88e14575d186fabfab8602240665c3cd9b44ed60
Okan Aras <oknaras@icloud.com>
2024-09-13 18:18:27 +0300

    mixins ve composables kullanım örneği

--- Ep_16/src/views/AboutView.vue
+++ Ep_16/src/views/AboutView.vue
@@ -1,15 +1,17 @@
+<!--options api-->
+<script setup>
+
+import {useFlash} from "@/composables/useFlash.js";
+
+let {flash} = useFlash();
+
+</script>
+
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
+  <div>
+    <p>
+      <button @click="flash('ABOUT','It Works on the about page','info')">Click Me</button>
+    </p>
   </div>
 </template>
 
-<style>
-@media (min-width: 1024px) {
-  .about {
-    min-height: 100vh;
-    display: flex;
-    align-items: center;
-  }
-}
-</style>
--- Ep_16/src/views/HomeView.vue
+++ Ep_16/src/views/HomeView.vue
@@ -1,50 +1,17 @@
 <!--options api-->
-<!--<script>-->
-<!--import TheWelcome from '../components/TheWelcome.vue'-->
-
-<!--export default {-->
-<!--  components: {-->
-<!--    TheWelcome-->
-<!--  },-->
-
-<!--  data() {-->
-<!--    return {-->
-<!--      message: 'Hello World',-->
-<!--    };-->
-<!--  },-->
-
-<!--  mounted() {-->
-<!--    alert('ive been mounted')-->
-<!--  }-->
-<!--};-->
-<!--</script>-->
-
-<!--compositons api-->
-
 <script setup>
-import TheWelcome from '../components/TheWelcome.vue'
-import {ref} from "vue";
 
-let message = ref('Hello World');
+import {useFlash} from "@/composables/useFlash.js";
 
-let doSomething = () => {
-  alert('got it');
-};
+let {flash} = useFlash();
 
 </script>
 
 
 <template>
   <main>
-    <TheWelcome/>
-
-    {{ message }}
-
     <p>
-      <input v-model="message" type="text"/>
-    </p>
-    <p>
-      <button @click="doSomething">Click HERE</button>
+      <button @click="flash('HOME','It Works on the home page')">Click Me</button>
     </p>
   </main>
 </template>

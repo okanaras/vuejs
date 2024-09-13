5cc580cb6104c8b398cc46289095f11b3cf1290b
Okan Aras <oknaras@icloud.com>
2024-09-13 17:53:22 +0300

    options api ile composition api farkı

--- Ep_16/src/views/HomeView.vue
+++ Ep_16/src/views/HomeView.vue
@@ -1,9 +1,50 @@
+<!--options api-->
+<!--<script>-->
+<!--import TheWelcome from '../components/TheWelcome.vue'-->
+
+<!--export default {-->
+<!--  components: {-->
+<!--    TheWelcome-->
+<!--  },-->
+
+<!--  data() {-->
+<!--    return {-->
+<!--      message: 'Hello World',-->
+<!--    };-->
+<!--  },-->
+
+<!--  mounted() {-->
+<!--    alert('ive been mounted')-->
+<!--  }-->
+<!--};-->
+<!--</script>-->
+
+<!--compositons api-->
+
 <script setup>
 import TheWelcome from '../components/TheWelcome.vue'
+import {ref} from "vue";
+
+let message = ref('Hello World');
+
+let doSomething = () => {
+  alert('got it');
+};
+
 </script>
+
 
 <template>
   <main>
-    <TheWelcome />
+    <TheWelcome/>
+
+    {{ message }}
+
+    <p>
+      <input v-model="message" type="text"/>
+    </p>
+    <p>
+      <button @click="doSomething">Click HERE</button>
+    </p>
   </main>
 </template>
